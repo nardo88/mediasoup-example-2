48afe9a86a6afdf92600749ec1e34c85e4cf1342
nardo88 <nardo1988@mail.ru>
2024-09-17 21:19:17 +0300

    Настроил клиент

---
+++ client/src/features/Example/Example.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react'
+import cls from './Example.module.scss'
+
+export const Example: FC = () => {
+  return <div className={cls.Example}>Example</div>
+}
---
+++ client/src/widget/App/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import './App.scss'
+import { Example } from '@features/Example/Example'
+
+export const App: FC = () => {
+  return (
+    <div>
+      <Example />
+    </div>
+  )
+}
